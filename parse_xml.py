d1381fade27c0891287513553e7859f06e444c80
johaywood <josh.haywood1@gmail.com>
2015-04-27 14:19:45 -0400

    Cleanup some code and add comments

--- parse_xml.py
+++ parse_xml.py
@@ -12,36 +12,37 @@ class AutoVivification(dict):
             return value
 
 #Generate tree in memory from XML file
-tree = ET.parse(sys.argv[1])
-root = tree.getroot()
+tree = ET.parse(sys.argv[1])      ## Individual animal data file
+root = tree.getroot()             ## Get root for individual animal data file
+grp_tree = ET.parse(sys.argv[2])  ## Group data file
+grp_root = grp_tree.getroot()     ## Get root for group data file
 
 #Initialize lists and dicts
-animal_uars = []
-meas_ids = []
-meas_by_day = []
-grp_ids = []
-grp_summary_ids = []
-group_grp_ids = []
-grp_meas_by_day = []
-animal_key = AutoVivification()
-an_grp_key = AutoVivification()
-grp_key = AutoVivification()
-group_grp_key = AutoVivification()
-meas_key = AutoVivification()
-meas_by_day_key = AutoVivification()
-grp_summary_key = AutoVivification()
-results = AutoVivification()
-grp_results = AutoVivification()
+animal_uars = []                        ## Individual animal ID list
+meas_ids = []                           ## Individual measurement ID list
+meas_by_day = []                        ## Individual animal measurements by day (eg. Bodyweight Day 1, Bodyweight Day 6, etc)
+grp_ids = []                            ## Individual animal file group IDs
+grp_summary_ids = []                    ## Group statistics summary IDs (eg. Mean, SEM, N, etc)
+group_grp_ids = []                      ## Group file group IDs
+grp_meas_by_day = []                    ## Group animal measurements by day (eg. Bodyweight Day 1, Bodyweight Day 6, etc)
+animal_key = AutoVivification()         ## {Animal UAR : Animal Name}
+an_grp_key = AutoVivification()         ## {Animal UAR : Animal Group}
+grp_key = AutoVivification()            ## Individual animal file {Group ID : Group Name}
+group_grp_key = AutoVivification()      ## Group file {Group ID : Group Name}
+meas_key = AutoVivification()           ## Individual animal file {Measurement ID : Measurement Name}
+grp_summary_key = AutoVivification()    ## Group summary stats key {Summary ID : Summary Name} (eg. {17 : Mean})
+results = AutoVivification()            ## Dictionary of individual animal results { UAR : { meas_by_day : { RESULT_STRING : result } } }
+grp_results = AutoVivification()        ## Dictionary of group results { grp_meas_by_day : { group_grp_id : { grp_summary_id : { RESULT_STRING : result } } } }
 
 study_number = str()
 study_title = str()
 
-#Collect study info
+#Collect study number and title
 for sn in root.iter('STUDY'):
   study_number = str(sn.find('STUDY_REFERENCE').text)
   study_title = str(sn.find('STUDY_TITLE').text)
 
-#Collect animal ID info
+#Collect individual animal ID and group ID info
 for anim in root.iter('ANIMAL'):
   anim_uar = str(anim.find('ANIMAL_UAR').text)
   anim_num = str(anim.find('ANIMAL_REFERENCE').text)
@@ -65,14 +66,14 @@ for grp in root.iter('GROUP'):
   grp_key[grp_id] = grp_name
   
 #Collect group file group ID info
-for grp in root.iter('GROUP'):
+for grp in grp_root.iter('GROUP'):
   grp_id = str(grp.find('GROUP_ID').text)
   grp_name = str(grp.find('GROUP_LONG_NAME').text)
   group_grp_ids.append(grp_id)
   group_grp_key[grp_id] = grp_name
   
 #Collect group summary ID info
-for summ in root.iter('GROUP_SUMMARY'):
+for summ in grp_root.iter('GROUP_SUMMARY'):
   summ_id = str(summ.find('GROUP_SUMMARY_ID').text)
   summ_name = str(summ.find('GROUP_SUMMARY_DESCR').text)
   grp_summary_ids.append(summ_id)
@@ -89,9 +90,9 @@ for result in root.iter('ANIMAL_RESULT'):
   for tp_to in result.iter('TIME_PERIOD_TO'):
     tpt = tp_to.text
   if tpf == tpt:
-    tp = meas_key[mid] + ' Day ' + tpf
+    tp = meas_key[mid] + ' Day ' + tpf ## Single day measurement
   else:
-    tp = meas_key[mid] + ' Day ' + tpf + ' - Day ' + tpt
+    tp = meas_key[mid] + ' Day ' + tpf + ' - Day ' + tpt ## Calculation over time measurement (eg. Day 1 - Day 6 change)
   meas_by_day.append(tp)
   for str_result in result.iter('RESULT_STRING'):
     val = str_result.text
@@ -100,7 +101,7 @@ meas_by_day = set(meas_by_day)
 meas_by_day = list(meas_by_day)
   
 #Collect group results by measurement, timepoint, group ID
-for result in root.iter('GROUP_SUMMARY_RESULT'):
+for result in grp_root.iter('GROUP_SUMMARY_RESULT'):
   for grp_id_result in result.iter('GROUP_ID'):
     gn = grp_id_result.text
     group_grp_ids.append(gn)
@@ -162,9 +163,7 @@ for x in range (0, len(grp_meas_by_day)):
 for x in range (0, len(grp_meas_by_day)):
   for y in range (0, len(group_grp_ids)):
     for z in range (0, len(grp_summary_ids)):
-      if grp_results[grp_meas_by_day[x]][group_grp_ids[y]][grp_summary_ids[z]]['RESULT_STRING'] == {}:
-        ws2.cell(row=(((x+1)*7+x+z)-4), column=y+2).value = ''
-      else:
+      if grp_results[grp_meas_by_day[x]][group_grp_ids[y]][grp_summary_ids[z]]['RESULT_STRING'] != {}:
         ws2.cell(row=(((x+1)*7+x+z)-4), column=y+2).value = grp_results[grp_meas_by_day[x]][group_grp_ids[y]][grp_summary_ids[z]]['RESULT_STRING']
 
 
